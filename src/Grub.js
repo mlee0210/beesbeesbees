b72ef801b79a4b8a5dd1d381e17493f47adce2f0
Michael Lee <lee.cs.michael@gmail.com>
2018-06-18 11:30:17 -0700

    Completed: Grub class functionality

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,44 @@
 var Grub = function() {
+	this.age = 0;
+	this.color = 'pink';
+	this.food = 'jelly';
+
 };
 
+Grub.prototype.eat = function() {
+
+}
+
+//
+
+//describe('Grub class functionality', function() {//
+
+//  verifyClass(Grub).followsPattern('pseudoclassical', {}, false);//
+
+//  beforeEach(function() {
+//    grub = new Grub();
+//  });//
+
+//  it('should have an `age` property that is set to `0`', function() {
+//    expect(grub.age).to.equal(0);
+//  });//
+
+//  it('should have a `color` property that is set to `pink`', function() {
+//    expect(grub.color).to.equal('pink');
+//  });//
+
+//  it('should have a `food` property that is set to `jelly`', function() {
+//    expect(grub.food).to.equal('jelly');
+//  });//
+
+//  it('should have an `eat` method', function() {
+//    expect(grub.eat).to.be.a('function');
+//  });//
+
+//});//
+//
+
+////an age property that is set to 0
+////a color property that is set to pink
+////a food property that is set to jelly
+////an eat method
